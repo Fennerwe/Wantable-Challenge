71e3feae656e94cf03bce91fd9a08b32d3e2a9f9
Fennerwe <fennerwe13@gmail.com>
2020-08-04 23:36:06 -0500

    Set up common api function

---
+++ src/api/apiCommon.ts
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import _ from 'lodash'
+import qs from 'qs'
+import { config } from './apiConfig'
+
+interface FetchOptions {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  data?: Object
+}
+
+export async function apiFetch<T>(
+  url: string,
+  options: FetchOptions,
+  queryParams?: Object
+): Promise<T> {
+  _.defaults(options, { method: 'GET' })
+  _.defaults(queryParams, { api_key: config.apiKey })
+
+  if (queryParams) {
+    url += qs.stringify(queryParams, { indices: false })
+  }
+
+  const result = await axios({
+    url,
+    ...options,
+  })
+
+  if (result.status !== 200) {
+    // error handling
+  }
+
+  return result.data
+}
---
+++ src/api/apiConfig.ts
@@ -0,0 +1,3 @@
+export const config = {
+  apiKey: '3bfadd3fcd70dca192723de71f9b5bad',
+}
