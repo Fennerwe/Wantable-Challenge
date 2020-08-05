78c3dce5b095ca07470d133adcc2a7414074db49
Fennerwe <fennerwe13@gmail.com>
2020-08-04 23:12:10 -0500

    Set up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './App.scss'
+import { Switch, Route, Redirect, HashRouter } from 'react-router-dom'
+import { Home } from './pages/home/Home'
 
 function App() {
   return (
     <div className="App">
-      <div>Start</div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   )
 }
