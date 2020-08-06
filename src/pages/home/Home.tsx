727d1a7908ccfd397cf45aad0ed39c4b82da993d
Fennerwe <fennerwe13@gmail.com>
2020-08-06 00:18:32 -0500

    Layout for artist cards

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom'
 import * as apiArtists from '../../api/artists/apiArtists'
 import { ArtistSearchResponse } from '../../api/artists/ArtistSearchResponse'
 import { Card } from '../../components/card/Card'
+import * as classes from './Home.module.scss'
 
 export const Home = (props: {}) => {
   const [artistName, setArtistName] = useState('')
@@ -13,42 +14,50 @@ export const Home = (props: {}) => {
   const history = useHistory()
 
   return (
-    <div>
-      <input
-        type="text"
-        value={artistName}
-        onChange={(ev) => setArtistName(ev.target.value)}
-        onKeyPress={async (ev) => {
-          if (ev.key === 'Enter') {
-            const result = await apiArtists.searchArtist(artistName)
-            if (result) {
-              setResults(result)
+    <div className={classes.pageContainer}>
+      <div className={classes.searchBarContainer}>
+        <input
+          className={classes.searchBar}
+          type="text"
+          placeholder="Search for an artist..."
+          value={artistName}
+          onChange={(ev) => setArtistName(ev.target.value)}
+          onKeyPress={async (ev) => {
+            if (ev.key === 'Enter') {
+              const result = await apiArtists.searchArtist(artistName)
+              if (result) {
+                setResults(result)
+              }
             }
-          }
-        }}
-      />
-      {!_.isEmpty(results) &&
-        _.map(results.results.artistmatches.artist, (artist, idx) => (
-          <Card
-            key={idx}
-            width="300px"
-            onClick={() => {
-              history.push(`artist/${artist.name}`)
-            }}
-          >
-            <div>
-              {artist.name}
-              <img
-                src={
-                  _.find(artist.image, (image) => image.size === 'large')?.[
-                    '#text'
-                  ]
-                }
-                alt={artist.name}
-              />
-            </div>
-          </Card>
-        ))}
+          }}
+        />
+      </div>
+
+      {!_.isEmpty(results) && (
+        <div className={classes.cardContainer}>
+          {_.map(results.results.artistmatches.artist, (artist, idx) => (
+            <Card
+              key={idx}
+              width="300px"
+              onClick={() => {
+                history.push(`artist/${artist.name}`)
+              }}
+            >
+              <div>
+                {artist.name}
+                <img
+                  src={
+                    _.find(artist.image, (image) => image.size === 'large')?.[
+                      '#text'
+                    ]
+                  }
+                  alt={artist.name}
+                />
+              </div>
+            </Card>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
