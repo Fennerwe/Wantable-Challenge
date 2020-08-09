0043486653c3866ed41d3dae5b413d1e3b855db8
Fennerwe <fennerwe13@gmail.com>
2020-08-09 02:41:01 -0500

    Test card component

---
+++ src/components/card/Card.test.tsx
@@ -0,0 +1,44 @@
+import { fireEvent, render } from '@testing-library/react'
+import React from 'react'
+
+import { Card } from './Card'
+
+describe('Card', () => {
+  it('renders without crashing', () => {
+    render(<Card cardTitle="Test" />)
+  })
+
+  it('fire the onclick event if passed in', () => {
+    const mockFn = jest.fn()
+    const el = render(<Card cardTitle="Test" onClick={mockFn} />)
+
+    const card = el.queryByRole('button')
+    if (card) {
+      fireEvent.click(card)
+    }
+
+    expect(mockFn).toHaveBeenCalled()
+  })
+
+  it('shows the image if a url is passed in', () => {
+    const testTitle = 'TestTitle'
+    const el = render(<Card cardTitle={testTitle} imgUrl="testImgUrl" />)
+
+    const img = el.queryByAltText(testTitle)
+    const noImageFound = el.queryByText('No Image Found')
+
+    expect(noImageFound).toBeNull()
+    expect(img).not.toBeNull()
+  })
+
+  it('shows "No Image Found" if no url is passed in', () => {
+    const testTitle = 'TestTitle'
+    const el = render(<Card cardTitle={testTitle} />)
+
+    const img = el.queryByAltText(testTitle)
+    const noImageFound = el.queryByText('No Image Found')
+
+    expect(noImageFound).not.toBeNull()
+    expect(img).toBeNull()
+  })
+})
